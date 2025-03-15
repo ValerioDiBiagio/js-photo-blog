fb838e566f2f96fe88a3ffb31d02bb15a18186e1
ValerioDiBiagio <dibiagiovalerio@gmail.com>
2025-03-15 18:54:34 +0100

    feat: add for and catch error

--- main.js
+++ main.js
@@ -10,4 +10,17 @@ axios.get(endpoint)
         const element = response.data
         console.log(element);
 
-})
+        // fare un ciclo for per iterare l'array
+        for (let i = 0; i < element.length; i++) {
+            // salvare in una variabile l'oggetto singolo dell'array
+            const currentElement = element[i]
+            console.log(currentElement);
+            // salvare in una variabile l'url dell'immagine presente nell'oggetto
+            const { url: imgUrl } = currentElement;
+        }
+
+    })
+    // se ci sono errori genero il messaggio di alert
+    .catch(error => {
+        console.log(alert('Errore'));
+    })
